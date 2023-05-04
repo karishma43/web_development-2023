393f0b511e56e21a13ec5b3e59fde8d7e65394c9
Karishma <patilkarishma1112@gmail.com>
2023-05-04 14:05:34 +0530

    html and css

---
+++ javascript basics/script.js
@@ -0,0 +1,17 @@
+
+// alert("hello world");
+//helps to take input values
+var age = prompt("whats your age?",18)
+// to check the values
+console.log(typeof age)
+
+if(age>=18){
+    console.log("user is eligible")
+}
+else{
+    console.log("user is not eligible")
+}
+
+var radius = prompt("radius")
+console.log("radius:")
+
